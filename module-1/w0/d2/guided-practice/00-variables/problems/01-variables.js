cab68681633538fb0243f2328e51e19b9a8d2401
brandon <brandonlaursen95@yahoo.com>
2024-09-17 11:44:07 -0400

    notes

--- module-1/w0/d2/guided-practice/00-variables/problems/01-variables.js
+++ module-1/w0/d2/guided-practice/00-variables/problems/01-variables.js
@@ -6,35 +6,67 @@ test your work by running your code and the test specs!
 // 1. Declare a variable called `firstName` and assign it your first name
 //    as a string:
 
-// Your code here 
+// let firstName = 'brandon';
+// console.log(firstName);
+// console.log('brandon');
+let firstName;// variable declaration
+
+firstName = 'brandon';// variable assignments
 
 
 // 2. Declare a variable called `lastName` and assign it your last name
 //    as a string:
 
-// Your code here 
-
+let lastName = 'laursen';// variable initialization
 
 // 3. Declare a variable called `age`:
 
-// Your code here 
+let age;
+let boolean = true;
+let myFunc = function(){};
 
+// {
+//   let age = 10;
+// }
+
+// age = '5';
+
+// var number = 6;
+// var number = 7;
 
 // 4. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
-// Your code here 
+console.log(firstName);
+console.log(lastName);
+console.log(age);
 
 
 // 5. Assign the `age` variable to a number:
-
-// Your code here 
+age = 5;
+console.log("age:", age);
 
 
 // 6. Print out the `firstName`, `lastName`, and `age` variables. What
 //    do you expect them to be when they get printed?
 
-// Your code here 
+console.log('===>',firstName, lastName, age);// ===> brandon laursen 5
+
+// console.log(first)
+
+// 'brandon laursen is 5';
+// * we are storing in a variable an expression
+// ! NOT
+// ? huh?
+
+let fullName = firstName + ' ' + lastName + ' is ' + age;
+
+let expression = 1 + 2 + 3 + 4 + 5;
+// console.log('expression: ',expression);// 15
+
+// console.log(fullName);// brandon laursen is 5
+
+let gravity = 9.8;
 
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
