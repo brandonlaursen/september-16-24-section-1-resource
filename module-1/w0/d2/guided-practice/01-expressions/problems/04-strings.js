eee665189b97223664223ea407c7509bf0f8a709
brandon <brandonlaursen95@yahoo.com>
2024-09-17 16:19:12 -0400

    strings

--- module-1/w0/d2/guided-practice/01-expressions/problems/04-strings.js
+++ module-1/w0/d2/guided-practice/01-expressions/problems/04-strings.js
@@ -1,6 +1,60 @@
 /*
 Let's practice playing with strings in JavaScript!
+
+  What are strings?
+  * Strings are delineated with "", or '' or `` which we learn next week
+  * Collection of characters in memory
+
+  Wrapper objects
+  * When we treat a primitive value like it was an object (i.e. by accessing properties and methods), JavaScript creates, under the hood, a wrapper to wrap this value and expose it as an object. The JS engine never reuses a wrapper object, giving them to the garbage collector right after a single use.
+
+  * https://programmingwithmosh.com/javascript/javascript-wrapper-objects/
+
+
+
 */
+
+let string = 'this is a string';
+let string2 = "cats";
+let string3 = `dog`;
+
+// d o g undefined
+// 0 1 2   3
+console.log(string3);// dog
+
+console.log(`dog`[0]);// d
+console.log(string3[0]);// d
+console.log(string3[1]);// o
+console.log(string3[2]);// g
+console.log('==>',string3[3]);
+
+// length property
+console.log(string3.length);// 3
+
+// `dog`[2]
+console.log(string3[string3.length - 1]);// g
+
+// string3[2] = g
+console.log(string3[1 + 1]);// ???
+
+
+let string4 = `Hello`;
+let name = 'brandon';
+
+let sentence = string4 + ' ' + name;
+console.log('==>',sentence);
+
+let sentence1 = `Hello ${name} has ${string2}`
+// console.log(sentence1);
+
+let characters = 'abc';
+// console.log(characters.length)
+
+console.log(characters.indexOf('a'));
+
+let result = characters.indexOf('a');
+console.log(result);
+
 
 // 1. Set the value of expression1 to the concatenated string
 //	  of str1A and str1B with a single space in between the
