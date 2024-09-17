22192f0d9c1c9fe1a702108c39b62abb19b56793
brandon <brandonlaursen95@yahoo.com>
2024-09-17 13:30:48 -0400

    booleans

--- module-1/w0/d2/guided-practice/01-expressions/problems/02-booleans.js
+++ module-1/w0/d2/guided-practice/01-expressions/problems/02-booleans.js
@@ -21,22 +21,22 @@ Let's practice playing with boolean expressions and operators in JavaScript!
 // false = 0
 //       true && false
 //          1 * 0 = 0
-console.log(1 && 0);
+// console.log(1 && 0);
 
 //        true || false
 //          1 + 0 = 1
-console.log(1 || 0);
+// console.log(1 || 0);
 
-console.log(false);
-console.log(true);
-console.log(!true);
-console.log(!false);
-console.log(!true || false);
-console.log(!true || !false);
-let result = !true || !false;
+// console.log(false);
+// console.log(true);
+// console.log(!true);
+// console.log(!false);
+// console.log(!true || false);
+// console.log(!true || !false);
+// let result = !true || !false;
 // console.log('result: ',result);
 
-let demorgans = !(true && false);
+// let demorgans = !(true && false);
 // let demorgans1 = !(
 //   function () {
 //     return true;
@@ -47,21 +47,25 @@ let demorgans = !(true && false);
 // );
 // (!true || !false);
 
+
+
+
+
 // 1. Set the value of expression1 to be true or false
 
-let expression1;
+let expression1 = true || false;
 
 console.log(expression1); //=> true
 
 // 2. Set the value of expression2 to be false and false
 
-let expression2;
+let expression2 = false && false;
 
 console.log(expression2); //=> false
 
 // 3. Set the value of expression3 to be false or true and true
 
-let expression3;
+let expression3 = false || true && true;
 
 console.log(expression3); //=> true
 
@@ -70,30 +74,36 @@ console.log(expression3); //=> true
 //    you think expression4 will evaluate to.
 let expression4 = true || (true && false);
 
-let predictExpression4;
+let predictExpression4 = true;
 
 // 5. Predict what the value of expression5 will be when printed
 //    to the console. Set predictExpression5 to the value that
 //    you think expression5 will evaluate to.
 let expression5 = (true || false) && false;
 
-let predictExpression5;
+let predictExpression5 = false;
 
 // 6. Predict what the value of expression6 will be when printed
 //    to the console. Set predictExpression6 to the value that
 //    you think expression6 will evaluate to.
 let expression6 = !(true && false) || !(true || false);
 
-let predictExpression6;
+// (!true || !false) || (!true && !false)
+// (true) ||  (false)
+
+let predictExpression6 = true;
 
 // 7. Predict what the value of expression7 will be when printed
 //    to the console. Set predictExpression7 to the value that
 //    you think expression7 will evaluate to.
 let a = true;
 let b = false;
+// false || true || (false && !true && (!false || !true));
+// true
+
 let expression7 = b || a || (b && !a && (!b || !a));
 
-let predictExpression7;
+let predictExpression7 = true;
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = {
