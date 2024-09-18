862de0d8da3ab78a957cc32bc1d56e7b413a833a
brandon <brandonlaursen95@yahoo.com>
2024-09-18 13:10:39 -0400

    p1

--- module-1/w0/d3/guided-practice/05-problem-solving/problems/01-refactor-check-if-last-char-is-an-e.js
+++ module-1/w0/d3/guided-practice/05-problem-solving/problems/01-refactor-check-if-last-char-is-an-e.js
@@ -7,20 +7,32 @@ the function code block instead of three lines of code.
 
 // 1. Define a function with the name `checkIfLastCharIsAnE`.
 // 2. Define a single parameter for the function called `str`.
+
+
 function checkIfLastCharIsAnE(str) {
+    // console.log('1.',str);
     // 3. Find the last character of `str`.
+    // stance[5]
     let lastChar = str[str.length - 1];
+    // console.log('2.', lastChar);// e
+
     // 4. Transform the last character of `str` to a lowercase character
-    lastChar = lastChar.toLowerCase();
+    // lastChar = lastChar.toLowerCase();
+    // console.log('3.',lastChar);
+
     // 5. Return true if the last character of `str` is an 'e'. Otherwise
     //    return false.
-    return lastChar === 'e';
-}
+    // console.log('4.',lastChar === 'e')
 
-// console.log(checkIfLastCharIsAnE('stance')); //=> true
-// console.log(checkIfLastCharIsAnE('FLOAT'));  //=> false
-// console.log(checkIfLastCharIsAnE('PHONE'));  //=> true
+    // return lastChar === 'e';
+    // str.toLowerCase()[(str.length - 1)] === 'e'
+    return str[str.length - 1].toLowerCase() === 'e';
+};
 
+
+console.log(checkIfLastCharIsAnE('stance')); //=> true
+console.log(checkIfLastCharIsAnE('FLOAT'));  //=> false
+console.log(checkIfLastCharIsAnE('PHONE'));  //=> true
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = checkIfLastCharIsAnE;
