474da6a2bbeefb99e04d908c5b4731e7355f21d0
brandon <brandonlaursen95@yahoo.com>
2024-09-18 11:46:36 -0400

    functions

---
+++ module-1/w0/d3/lecture/functions.js
@@ -0,0 +1,83 @@
+
+
+
+
+// console.log('entering file')
+
+// function definition/declaration
+
+
+function myFunc(){
+  console.log('entering function');
+
+};
+
+
+// console.log('before function call');
+
+// function call/invocation
+// myFunc();
+
+// console.log('after function call');
+
+
+
+
+
+// parameters
+// let firstName = nameFirst = 'brandon'
+// let lastName = nameLast = 'laursen'
+// let age = undefined
+function myFunc2(firstName, lastName, age){
+
+  console.log('entering function2');
+  console.log(firstName, lastName, age);// brandon laursen;
+
+  // return undefined;
+};
+
+// let nameFirst = 'brandon';
+// let nameLast = 'laursen'
+
+// myFunc2(nameFirst, nameLast);
+
+
+// function myFunc3(num1, num2) {
+//   return num1 + num2;
+// }
+
+// myFunc2('brandon', 'laursen');
+
+// console.log('between function calls');
+
+// myFunc2('maica', 'santos', 29);
+
+
+// function definition
+function addNumbers(num1, num2) {
+
+  // console.log(num1 + num2);
+
+  // return true;
+  // return 'string';
+
+  // let result = num1 + num2;
+  // return result;
+
+  return num1 + num2;
+
+  console.log('will not be ran');
+};
+
+
+// function call
+// input - arguments
+// if you wanna see what a function evaluates too
+// wrap it in a console.log
+
+console.log('Return value of addNumbers: ', addNumbers(10, 5));
+console.log('Return value of addNumbers: ', addNumbers(1000, 15));
+// console.log('Return value of addNumbers: ', 10);
+
+// let returnValue = addNumbers(10, 20);
+// console.log(returnValue);
