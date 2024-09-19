e40a633929b0537af59fa7e98128fbc43615695d
brandon <brandonlaursen95@yahoo.com>
2024-09-19 11:19:02 -0400

    conditionals

---
+++ module-1/w0/d4/lecture/conditionals.js
@@ -0,0 +1,53 @@
+function myFunc(num) {
+  console.log(num); // 10
+
+  // if(num >= 5) {
+  //   // console.log('num is greater than 5');
+  //   return 'num is greater than 5';
+  // } else {
+  //   // console.log('num is not greater than 5')
+  //   return 'num is not greater than 5'
+  // }
+
+  if (num >= 5) {
+    return "num is greater than 5";
+  }
+
+  return "num is not greater than 5";
+
+  console.log("will we reach here");
+}
+
+// console.log(myFunc(5));
+
+function myFunc2(num) {
+  // if (num > 10) {
+  //   console.log("number is greater than 10");
+  // } else if (num > 15) {
+  //   console.log("number is greater than 15");
+  // } else if (num < 10) {
+  //   console.log("number is less than 10");
+  // } else {
+  //   console.log("number is 10");
+  // };
+
+  // if(num > 10) {
+  //   console.log("number is greater than 10");
+  // }
+  // if(num > 15) {
+  //   console.log("number is greater than 15");
+  // }
+
+  if ("") {
+    console.log("this will not run");
+  }
+  if ("string") {
+    console.log("this will run");
+  }
+
+  if ([]) {
+    console.log("this will run");
+  }
+}
+
+console.log(myFunc2(20));
