2b0f1f7e66892da527c3cef73f3bad2da20e3438
brandon <brandonlaursen95@yahoo.com>
2024-09-19 13:16:40 -0400

    practice 3

--- module-1/w0/d4/guided-practice/02-basic-loops/problems/03-log-between.js
+++ module-1/w0/d4/guided-practice/02-basic-loops/problems/03-log-between.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 /*
 Define a function logBetween that takes in two number parameters. The first
 number parameter represents a low number and the second represents a high
@@ -6,9 +11,48 @@ high number, inclusive. Inclusive means that the range includes the low and
 high numbers.
 */
 
-// Your code here 
+/*
 
-// logBetween(-1, 2);
+  Understand the problem
+    input: two nums, lowNum = -1  highNum = 2
+    output: console.log() -1 1 0 1 2
+    * how do we print every num between the low and high
+
+  make a plan
+    1. Define a function logBetween two nums low and high
+    2. create a loop
+      a. start - lowNum
+      b. stop/condition - -1 <= 2  -> i <= 2
+      c. step - increment by 1
+      d. console.log(i)
+
+  execute the plan
+  refactor
+
+  for (let step = 0; step < 5; step++) {
+  console.log("Walking east one step");
+  }
+
+*/
+
+function logBetween(low, hi) {
+  // let i = 0;
+  // let num = 1;
+  // let index = num;
+
+  // console.log(low, hi);
+  // i = low = -1
+  // let num = 5;
+  // 3 <= 2
+  for(let i = low; i <= hi; i++){
+    console.log(i);
+  }
+}
+
+
+
+logBetween(-1, 2);// -1 2
+// logBetween(5, 20);
 /* prints out:
 -1
 0
