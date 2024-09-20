1839d8394d58dd9ba865feb34a594deeb5578824
brandon <brandonlaursen95@yahoo.com>
2024-09-20 11:50:41 -0400

    fib

--- module-1/w0/d4/guided-practice/02-basic-loops/problems/22-fibonacci-number.js
+++ module-1/w0/d4/guided-practice/02-basic-loops/problems/22-fibonacci-number.js
@@ -3,9 +3,52 @@ Write a function fib that accepts a number n as an argument. The function should
 return the "n-th" number of the Fibonacci sequence. The first two numbers of the
 Fibonacci sequence are 1; to generate subsequent numbers of the Fibonacci
 sequence, we take the sum of the previous two numbers of the sequence.
+
+
+  Understand the problem
+  Input: 6
+  Output: 8
+  1 2 3 4 5 6    7  8 9
+  1 1  2 3 5 8    13
+            ^
+
+  Make a plan
+  1. define a function takes in num
+  2. if the input is less than or equal 2 - we can return 1
+  3. define var for prev - 1
+  4. define var for current - 1
+  5. define temp var to hold old current - temp
+  4. iterate starting from 2, up to the n; in increments in 1
+    a. take the sum of curr + prev and assign it to temp
+    b. prev = curr
+    c. curr = temp
+  5. return the temp
+
+
+  Execute the plan
+  Refactor
 */
 
-// Your code here 
+
+function fib(num) {
+
+  if(num <= 2) return 1;
+
+  let prev = 1;
+  let curr = 1;
+  let temp;
+
+  for(let i = 2; i < num; i++) {
+    console.log('before change:', prev, curr)
+    temp = prev + curr;// 2
+    prev = curr;// 1
+    curr = temp;// 2
+
+    console.log("prev:", prev, "temp:", temp, "curr:", curr);
+  };
+
+  return temp;
+}
 
 // console.log(fib(1));    // 1
 // console.log(fib(2));    // 1
