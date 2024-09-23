1d9da9fe881c1db6822701b254e8f9d81598086a
brandon <brandonlaursen95@yahoo.com>
2024-09-23 16:29:08 -0400

    numba 9

--- module-1/w1/d1/guided-practice/02-mutability/problems/07-tripler.js
+++ module-1/w1/d1/guided-practice/02-mutability/problems/07-tripler.js
@@ -4,10 +4,33 @@ should return a new array containing three times every number of the original
 array.
 */
 
-// Your code here 
+// mutate input array
+function doubler(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let doubled = nums[i] * 2
+    nums[i] = doubled;
+  }
+
+  return nums; // [2, 4, 6, 8]
+}
 
 
-// console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
+// create a new array
+function tripler(nums) {
+
+  let newArr = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    // nums[i] = nums[i] * 3;
+    let tripled = nums[i] * 3
+    newArr.push(tripled);
+  }
+
+  return newArr ; //
+
+}
+
+console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
 // console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
--- module-1/w1/d1/guided-practice/02-mutability/problems/09-mirror.js
+++ module-1/w1/d1/guided-practice/02-mutability/problems/09-mirror.js
@@ -4,10 +4,32 @@ returns a new array where all the elements in the original array added to the
 new array twice with the second set of elements in reverse order.
 */
 
-// Your code here 
+
+function mirror(array){
+
+  let newArr = [];
+
+  for(let i = 0; i < array.length; i++){
+    newArr.push(array[i]);
+  };
+
+  // console.log(newArr)
+
+  for(let i = array.length - 1; i >= 0; i--){
+    newArr.push(array[i]);
+  }
+  // console.log(newArr);
+
+  // [1,2,3,3,2,1];
+  return array.slice().concat(  array.slice().reverse()   );
+
+  // return [...array, ...array.reverse()];
+
+  return newArr;
+}
 
 
-// console.log(mirror([1,2,3])); //=> [ 1, 2, 3, 3, 2, 1 ]
+console.log(mirror([1,2,3])); //=> [ 1, 2, 3, 3, 2, 1 ]
 // console.log(mirror(['a', 'b', 'c', 'd']));
 // //=> [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 
