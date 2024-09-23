c481e65c9945e55d40dfcb5f24d0e7ed06dc7f69
brandon <brandonlaursen95@yahoo.com>
2024-09-23 16:42:45 -0400

    number 10

--- module-1/w1/d1/guided-practice/02-mutability/problems/10-alternating-words.js
+++ module-1/w1/d1/guided-practice/02-mutability/problems/10-alternating-words.js
@@ -2,13 +2,46 @@
 Write a function alternatingWords that accepts an array of words as an argument.
 The function should mutate the input array such that the words alternate between
 fully uppercase or lowercase. The first word should be uppercase.
+
+input: [ 'Belka', 'STRELKA', 'laika', 'DEZIK' ];
+
+output: [ 'BELKA', 'strelka', 'LAIKA', 'dezik' ]
+
+    0        1         2        3        4
+'BELKA', 'strelka', 'LAIKA', 'dezik'
+
+0, 2 - upper cased
+1, 3 - lower cased
+
+* all words at even indexes are upper cased
+* all words at odd indexes are lowercase cased
+
 */
 
-// Your code here 
+function alternatingWords(words) {
 
-// let words1 = [ 'Belka', 'STRELKA', 'laika', 'DEZIK' ];
-// alternatingWords(words1);
-// console.log(words1); // [ 'BELKA', 'strelka', 'LAIKA', 'dezik' ]
+  // return words.map();
+
+  //      0        1         2         3
+  // [ 'BELKA', 'strelka', 'LAIKA', 'dezik' ];
+  // i = 4
+  for (let i = 0; i < words.length; i++) {
+    // let word = words[i];
+
+    if (i % 2 === 0) {
+      words[i] = words[i].toUpperCase();
+    } else {
+      words[i] = words[i].toLowerCase();
+    }
+  }
+
+  console.log(words);
+  return words;
+}
+
+let words1 = ["Belka", "STRELKA", "laika", "DEZIK"];
+alternatingWords(words1);
+console.log(words1); // [ 'BELKA', 'strelka', 'LAIKA', 'dezik' ]
 
 // let words2 = [ 'Yellowstone', 'Yosemite', 'Zion', 'Acadia', 'Shenandoah' ];
 // alternatingWords(words2);
