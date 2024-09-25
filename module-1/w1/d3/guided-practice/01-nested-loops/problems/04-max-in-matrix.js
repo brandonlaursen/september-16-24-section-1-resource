623673faf366314218e23ed95bfc18540a9c34f4
brandon <brandonlaursen95@yahoo.com>
2024-09-25 13:39:38 -0400

    04

--- module-1/w1/d3/guided-practice/01-nested-loops/problems/04-max-in-matrix.js
+++ module-1/w1/d3/guided-practice/01-nested-loops/problems/04-max-in-matrix.js
@@ -4,17 +4,41 @@ Write a function maxInMatrix(matrix) that takes in a 2-dimensional array
 least one value.
 */
 
+
+
+
+
 function maxInMatrix(matrix) {
-  // Your code here 
+
+  // 11 > -Infinity
+  let largestNum = -Infinity;
+
+  for(let i  = 0; i < matrix.length; i++){
+    let subArr = matrix[i];
+    // console.log(subArr);
+
+    for(let j = 0; j < subArr.length; j++){
+      let number = subArr[j];
+      // console.log("number:", number);
+
+      if(number > largestNum) {
+        largestNum = number;
+
+      };
+      // console.log("largestNum:", largestNum);
+    }
+  };
+
+  return largestNum;
 }
 
-// matrix = [
-//   [11, 2, -99],
-//   [20, 19, 10],
-//   [47, 72, 56],
-// ];
+matrix = [
+  [11, 2, -99],
+  [20, 19, 10],
+  [47, 72, 56],
+];
 
-// console.log(maxInMatrix(matrix)); // 72
+console.log(maxInMatrix(matrix)); // 72
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = maxInMatrix;
