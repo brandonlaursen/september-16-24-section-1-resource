c8ce35728fc49dfceb12ebefaff56ac782750f00
brandon <brandonlaursen95@yahoo.com>
2024-09-25 13:56:23 -0400

    05

--- module-1/w1/d3/guided-practice/01-nested-loops/problems/05-max-in-columns.js
+++ module-1/w1/d3/guided-practice/01-nested-loops/problems/05-max-in-columns.js
@@ -6,16 +6,39 @@ each of its elements is the max value in a column.
 */
 
 function maxColumn(matrix) {
-  // Your code here 
+  // Your code here
+
+  let height = matrix.length;
+  let width = matrix[0].length;
+  let result = [];
+
+  for (let col = 0; col < width; col++) {
+    let colMax = matrix[0][col];
+    // console.log('before inner loop',colMax);
+
+    for (let row = 0; row < height; row++) {
+      // let number = matrix[col][row];
+      let number = matrix[row][col];
+      // console.log(number)
+      if (number > colMax) {
+        colMax = number;
+      }
+      // console.log('in inner loop',colMax)
+    }
+    // console.log('after inner loop', colMax)
+    result.push(colMax);
+  }
+
+  return result;
 }
 
-// matrix = [
-//   [5, 9, 21],
-//   [9, 19, 6],
-//   [12, 14, 15],
-// ];
+matrix = [
+  [5, 9, 21],
+  [9, 19, 6],
+  [12, 14, 15],
+];
 
-// console.log(maxColumn(matrix)); // [12, 19, 21]
+console.log(maxColumn(matrix)); // [12, 19, 21]
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = maxColumn;
