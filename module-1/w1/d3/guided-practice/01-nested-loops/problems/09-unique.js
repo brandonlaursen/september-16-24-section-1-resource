56d71c8c47b6a3dac64df1f98efbbdcf03ef720f
brandon <brandonlaursen95@yahoo.com>
2024-09-26 11:16:44 -0400

    unique

--- module-1/w1/d3/guided-practice/01-nested-loops/problems/09-unique.js
+++ module-1/w1/d3/guided-practice/01-nested-loops/problems/09-unique.js
@@ -4,7 +4,21 @@ should return a new array containing elements of the input array, without
 duplicates.
 */
 
-// Your code here 
+
+function unique(arr) {
+  let newArr = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    let ele = arr[i];
+    // console.log(ele)
+    if(!newArr.includes(ele)) {
+      newArr.push(ele);
+    }
+
+  }
+  // console.log(newArr);
+  return newArr;
+}
 
 // console.log(unique([1, 1, 2, 3, 3]));         // [1, 2, 3]
 // console.log(unique([11, 7, 8, 10, 8, 7, 7])); // [11, 7, 8, 10]
