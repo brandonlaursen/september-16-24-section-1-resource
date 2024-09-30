6955d058b98f78240ad360d8443810913492466d
brandon <brandonlaursen95@yahoo.com>
2024-09-30 19:32:55 -0400

    05 object to array sum

--- module-1/w2/d1/guided-practices/02-pojo-iteration/problems/05-object-to-array-sum.js
+++ module-1/w2/d1/guided-practices/02-pojo-iteration/problems/05-object-to-array-sum.js
@@ -5,15 +5,50 @@ nested array is formatted such that the first element is the key and the second
 element is the sum of all the numbers in the array value.
 */
 
-// Your code here 
 
-// let populations = {
-//   neighborhood1: [10, 1, 3, 6],
-//   neighborhood5: [],
-//   neighborhood8: [5, 0, 3, 2]
-// }
-// console.log(objectToArraySum(populations));
-// //=> [['neighborhood1', 20], ['neighborhood5', 0], ['neighborhood8', 10]]
+
+let objectToArraySum = (obj) =>{
+  // console.log(obj);
+
+  let result = [];
+
+  let keysArr = Object.keys(obj);
+  // console.log(keysArr);// [ 'neighborhood1', 'neighborhood5', 'neighborhood8' ]
+
+  for(let i = 0; i < keysArr.length; i++){
+    let key = keysArr[i];
+    // console.log(key);
+    let valueArr = obj[key];// [ 10, 1, 3, 6 ]
+    // console.log(valueArr);
+    // ['neighborhood1']
+    // console.log(valueArr);
+    let sum = 0;
+    for(let num of valueArr) {
+      // console.log(num);
+      sum += num;
+    }
+    // console.log(sum);
+
+
+    let subArr = [key, sum];
+      // console.log(subArr);
+    result.push(subArr);
+  }
+
+  return result;
+}
+
+let populations = {
+  neighborhood1: [10, 1, 3, 6],
+  neighborhood5: [],
+  neighborhood8: [5, 0, 3, 2]
+};
+
+console.log(objectToArraySum(populations));
+//=> [  ['neighborhood1', 20],    ['neighborhood5', 0], ['neighborhood8', 10]]
+
+
+
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 
