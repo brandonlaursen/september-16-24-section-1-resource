0bcd0c4cb6fa064ba5adfbc485ff78fb37770960
brandon <brandonlaursen95@yahoo.com>
2024-09-30 19:46:45 -0400

    11

--- module-1/w2/d1/guided-practices/02-pojo-iteration/problems/11-is-palindrome.js
+++ module-1/w2/d1/guided-practices/02-pojo-iteration/problems/11-is-palindrome.js
@@ -5,19 +5,42 @@ and assume all characters are lowercase.
 
 Note: A palindrome is a sequence that reads the same backwards as is does
 forward.
+
+let = {
+'o': 1
+}
+
+    abab
+let = {
+}
+
 */
 
-const isPalindrome = sentence => {
-  // Your code here 
-}
+const isPalindrome = (sentence) => {
+  let obj = {};
+
+  for (let char of sentence) {
+    // console.log(char);
+
+    if (obj[char] === undefined) {
+      obj[char] = 1;
+    } else {
+      delete obj[char];
+    }
+  }
+
+  console.log(obj);
+  let count = Object.values(obj).length;
+  // console.log(count);
+  return count <= 1;
+};
 
 // console.log(isPalindrome('pop'));               // true;
 // console.log(isPalindrome('kayak'));             // true
 // console.log(isPalindrome('yo banana boy'));     // true
-// console.log(isPalindrome('this is the truth')); // false
+// console.log(isPalindrome('this is the truth')); // false ->
 // console.log(isPalindrome('abab'));              // true
-// // because 'abab' can be rearranged into a palindrome, 'abba'
-
+// because 'abab' can be rearranged into a palindrome, 'abba'
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 
