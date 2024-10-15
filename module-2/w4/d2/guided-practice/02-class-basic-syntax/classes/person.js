7355f0d902ede989ec4320dc75a37f3b54225a39
brandon <brandonlaursen95@yahoo.com>
2024-10-15 14:06:33 -0400

    classes pt1

--- module-2/w4/d2/guided-practice/02-class-basic-syntax/classes/person.js
+++ module-2/w4/d2/guided-practice/02-class-basic-syntax/classes/person.js
@@ -1,4 +1,50 @@
-// Your code here 
+
+
+
+
+
+
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+
+  }
+
+
+  introduce(){
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+  }
+
+  static introducePeople(people) {
+    // console.log(people);
+    if(!Array.isArray(people)) {
+      // console.log('throw an error')
+      throw new Error( "introducePeople only takes an array as an argument.")
+    };
+
+    for(let person of people) {
+
+      if(!(person instanceof Person)) {
+        throw new Error("All items in array must be Person class instances.")
+      }
+      console.log(person.introduce());
+    }
+  }
+}
+
+
+
+const person = new Person("kristen", "chauncey", 38);
+const person2 = new Person("jeff", "granoff", 30);
+
+// Person.introducePeople([person, person2]);
+// Person.introducePeople('hello');
+// console.log("person:", person);
+// person: Person { firstName: 'kristen', lastName: 'chauncey', age: 38 }
+
+// person.introduce();
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- module-2/w4/d2/guided-practice/02-class-basic-syntax/test/person-spec.js
+++ module-2/w4/d2/guided-practice/02-class-basic-syntax/test/person-spec.js
@@ -63,7 +63,7 @@ describe("Person class", () => {
     it('should throw an error if array does not contain instances of Person', () => {
       let badInput = ['dog', person]
 
-      expect(() => Person.introducePeople(badInput)).to.throw(Error, 
+      expect(() => Person.introducePeople(badInput)).to.throw(Error,
         "All items in array must be Person class instances."
       );
     })
