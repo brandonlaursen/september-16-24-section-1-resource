2ebb82343fff9224f202780e50b72268ec35f9b4
brandon <brandonlaursen95@yahoo.com>
2024-10-15 16:52:48 -0400

    inheritence

--- module-2/w4/d2/guided-practice/03-class-inheritance/classes/teacher.js
+++ module-2/w4/d2/guided-practice/03-class-inheritance/classes/teacher.js
@@ -1,7 +1,27 @@
 const Person = require('./person');
 
-// Your code here 
 
+
+
+
+class Teacher extends Person{
+  constructor(firstName,lastName, subject,yearsOfExperience ){
+    super(firstName, lastName)
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    return teachers.reduce((sum, teacher) => sum += teacher.yearsOfExperience,0)
+  }
+}
+
+
+const teacher1 = new Teacher("susan", "jones", "biology", 5);
+const teacher2 = new Teacher("bobby", "roberts", "math", 15);
+// console.log("teacher1:", teacher1);
+
+console.log(Teacher.combinedYearsOfExperience([teacher1, teacher2]))
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
