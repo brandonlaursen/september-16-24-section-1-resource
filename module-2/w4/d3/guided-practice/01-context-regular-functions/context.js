69674ce1f8fd397a9c37fb9caf9c89b609806bbf
brandon <brandonlaursen95@yahoo.com>
2024-10-16 12:40:52 -0400

    01 context

--- module-2/w4/d3/guided-practice/01-context-regular-functions/context.js
+++ module-2/w4/d3/guided-practice/01-context-regular-functions/context.js
@@ -14,24 +14,32 @@ class Fish {
 
 const nemo = new Fish('Nemo');
 
+// const obj = {};
+// console.log(obj.name);// undefined
+
+// const ele = undefined;
+// console.log(ele.name);
+
 /********************************* Scenario 1 *********************************/
-// eat(); // ?
+eat(); // context is the global object
 
 
 /********************************* Scenario 2 *********************************/
 // nemo.eat = eat;
-// nemo.eat(); // ?
+// nemo.eat(); // context is nemo
 
 
 /********************************* Scenario 3 *********************************/
 // nemo.eat = eat;
-// eat(); // ?
-
+// eat(); // context is the global object
+// console.log(nemo)
 
 /********************************* Scenario 4 *********************************/
-// nemo.swim(); // ?
+// nemo.swim(); // context is nemo
 
 
 /********************************* Scenario 5 *********************************/
-// const swim = nemo.swim;
-// swim(); // ?
+const swim = nemo.swim;
+swim(); //  classes are by default in strict mode, when we try to key into the name property, we are keying into undefined
+// strict disables accessing the global object
+// undefined.name; 
