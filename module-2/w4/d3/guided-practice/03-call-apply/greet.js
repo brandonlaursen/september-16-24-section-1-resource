fb561285d5fa157dbbd0b121146101a7e0f647a9
brandon <brandonlaursen95@yahoo.com>
2024-10-16 13:53:45 -0400

    greet.js

--- module-2/w4/d3/guided-practice/03-call-apply/greet.js
+++ module-2/w4/d3/guided-practice/03-call-apply/greet.js
@@ -6,5 +6,10 @@ function say(name) {
 helloMessage = { message: 'Hello,' };
 heyThereMessage = { message: 'Hey there,' };
 
-say(helloMessage, "John");
-say(heyThereMessage, "Michael");
+// say(helloMessage, "John");
+// say(heyThereMessage, "Michael");
+
+
+
+say.call(helloMessage, "John");
+say.apply(heyThereMessage, ["Michael"]);
