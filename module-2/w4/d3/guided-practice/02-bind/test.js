5dc98389c5bb580c5a41b56c533cb6923020bfe7
brandon <brandonlaursen95@yahoo.com>
2024-10-16 12:55:54 -0400

    bind

--- module-2/w4/d3/guided-practice/02-bind/test.js
+++ module-2/w4/d3/guided-practice/02-bind/test.js
@@ -1,1 +1,31 @@
-// Your code here 
+"use strict";
+
+const Employee = require('./employee');
+
+
+
+const john = new Employee('John Wick', 'Dog Lover');
+
+// const myFunc = john.sayName;
+
+// setTimeout(john.sayName, 2000);//
+
+
+let sayNameBound = john.sayName.bind(john);
+setTimeout(sayNameBound, 2000);
+
+
+// class Cat {
+//   purr() {
+//     console.log("meow");
+//   }
+
+//   purrMore() {
+//     console.log(this.name);
+//     // this.purr();
+//   }
+// }
+// let cat = new Cat();
+
+// console.log(undefined());// console.log(undefined());
+// global.setTimeout(cat.purrMore, 5000);
