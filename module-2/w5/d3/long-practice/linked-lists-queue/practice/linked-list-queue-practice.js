ba3b8a2c8de826fc9abe58a80eaee3095952e015
brandon <brandonlaursen95@yahoo.com>
2024-10-24 11:42:30 -0400

    findMid

--- module-2/w5/d3/long-practice/linked-lists-queue/practice/linked-list-queue-practice.js
+++ module-2/w5/d3/long-practice/linked-lists-queue/practice/linked-list-queue-practice.js
@@ -10,6 +10,7 @@ class SinglyLinkedNode {
 class SinglyLinkedList {
     constructor(head = null) {
         this.head = head;
+        this.length = 0;
     }
 
     addToTail(val) {
@@ -17,6 +18,7 @@ class SinglyLinkedList {
 
         if (!this.head) {
             this.head = newNode;
+            this.length++;
             return this.head;
         }
 
@@ -26,6 +28,7 @@ class SinglyLinkedList {
         }
 
         curr.next = newNode;
+        this.length++;
         return this.head;
     }
 
@@ -33,13 +36,13 @@ class SinglyLinkedList {
         // Returns the length of the list
         // Implement in O(n) and in O(1) time complexity
 
-        // Your code here 
+        // Your code here
     }
 
     sumOfNodes() {
         // Returns the sum of the values of all the nodes
 
-        // Your code here 
+        // Your code here
 
         // Write your hypothesis on the time complexity of this method here
     }
@@ -47,7 +50,7 @@ class SinglyLinkedList {
     averageValue() {
         // Returns the average value of all the nodes
 
-        // Your code here 
+        // Your code here
 
         // Write your hypothesis on the time complexity of this method here
     }
@@ -55,19 +58,35 @@ class SinglyLinkedList {
     findNthNode(n) {
         // Returns the node at the nth index from the head
 
-        // Your code here 
+        // Your code here
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     findMid() {
-        // Returns the middle node
-        // Implement this as a singly linked list then as a doubly linked list
-            // How do the implementation for singly and doubly vary if at all?
 
-        // Your code here 
+        let curr = this.head;
 
-        // Write your hypothesis on the time complexity of this method here
+        let midIndex;
+
+        if(this.length % 2 === 0) {
+            midIndex = ((this.length / 2) - 1);
+        } else {
+            midIndex = Math.ceil((this.length / 2) - 1)
+        };
+
+        let index = 0;
+        while(curr) {
+
+            if(index === midIndex) {
+               return curr;
+            };
+            index++;
+            curr = curr.next;
+        }
+
+
+        return curr;
     }
 
 
@@ -77,7 +96,7 @@ class SinglyLinkedList {
         // the original linked list reversed in place
             // Does the time complexity change? How about space complexity?
 
-        // Your code here 
+        // Your code here
 
         // Write your hypothesis on the time complexity of this method here
     }
@@ -85,12 +104,21 @@ class SinglyLinkedList {
     reverseInPlace() {
         // Reverses the linked list in-place
 
-        // Your code here 
+        // Your code here
 
         // Write your hypothesis on the time complexity of this method here
     }
 
-}
+};
+
+const list = new SinglyLinkedList();
+list.addToTail(1);
+list.addToTail(2);
+list.addToTail(3);
+// list.addToTail(4);
+// list.addToTail(5);
+// list.addToTail(6);
+console.log(list.findMid());
 
 class DoublyLinkedNode {
     constructor(val) {
@@ -127,15 +155,15 @@ class DoublyLinkedList {
         // Implement this as a singly linked list then as a doubly linked list
             // How do the implementation for singly and doubly vary if at all?
 
-        // Your code here 
-        
+        // Your code here
+
         // Write your hypothesis on the time complexity of this method here
     }
 
     reverse() {
         // Returns a new reversed version of the linked list
 
-        // Your code here 
+        // Your code here
 
         // Write your hypothesis on the time complexity of this method here
     }
@@ -143,7 +171,7 @@ class DoublyLinkedList {
     reverseInPlace() {
         // Reverses the linked list in-place
 
-        // Your code here 
+        // Your code here
 
         // Write your hypothesis on the time complexity of this method here
     }
