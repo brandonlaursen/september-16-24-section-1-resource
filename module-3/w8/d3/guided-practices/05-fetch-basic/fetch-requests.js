187ef334ae62f46b6044b6e9ac9cbb086a1ba733
brandon <brandonlaursen95@yahoo.com>
2024-11-13 17:16:38 -0500

    fetch requests

--- module-3/w8/d3/guided-practices/05-fetch-basic/fetch-requests.js
+++ module-3/w8/d3/guided-practices/05-fetch-basic/fetch-requests.js
@@ -6,9 +6,12 @@ Paste your code for fetch requests here once you finish each task.
 /* =============== 1. Print the status code of the response =============== */
 
 (async function(){
+  // GET /products
   const res = await fetch('/products');
+  console.log(res);
+
   const data = await res.text();
-     console.log(data)
+  // console.log(data)
  })();
 
  fetch('/products').then((res) => res.text()).then((data) => console.log(data))
@@ -16,15 +19,32 @@ Paste your code for fetch requests here once you finish each task.
 /* ====== 2. Print true if the status of the response was successful ====== */
 
 // Your code here
+(async function(){
+  // GET /products
+  const res = await fetch('/products');
 
+  console.log(res.status)
+ })();
 
 
 /* =================== 3. Print the Content-Type Header =================== */
+(async function(){
+  // GET /products
+  const res = await fetch('/products');
 
-// Your code here
+  console.log(res.headers.get('Content-Type'))
+ })();
+
 
 
 
 /* ============== 4. Print the body of the response as text =============== */
 
-// Your code here
+(async function(){
+  // GET /products
+  const res = await fetch('/products');
+  console.log(res);
+
+  const data = await res.text();
+  console.log(data)
+ })();
---
+++ w8-key-points.js
@@ -0,0 +1,190 @@
+
+
+// Request - Response Cycle
+// * conversation between a client and a server
+// * client - frontend/postman/browser
+// * server/api
+
+
+// How can we send a request?
+// * using FETCH
+
+//* GET
+fetch("/resource");
+
+// * POST
+fetch("/resource", {
+  method: "POST",
+  headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  body: new URLSearchParams({ name: "little bear", age: 14 }),
+});
+
+// What does fetch return?
+// * A promise
+
+// .then
+fetch("/resource")
+  .then((data) => data.text())
+  .then((data) => console.log(data));
+
+
+// async await
+async function wrapper(){
+  const data = await fetch("/resource");
+  const text = data.text();
+  console.log(text);
+}
+
+
+
+// * What do we send fetch request too?
+// server's endpoints
+
+// The servers have endpoints
+// * method + url
+// * GET /resource
+
+
+// Methods
+// * Traditional web server
+// GET/POST
+
+
+// Endpoints follow restful convention
+// ! This is convention for tradtional web servers
+// * GET /resource
+// * POST /resource
+// * GET /resource/:id
+// * POST /resource/:id/edit
+// * POST /resource/:id/delete
+
+
+// Traditional Web Server
+// * REQUEST
+// * GET
+request = {
+  method: "GET",
+  URL: "/resource",
+  headers: false,
+  body: false,
+};
+
+response = {
+  headers: {
+    "Content-Type": "text/html",
+  },
+  statusCode: 200,
+  body: true,
+};
+
+// * POST
+request = {
+  method: "POST",
+  URL: "/resource",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  body: {
+    key: true,
+  },
+};
+
+response = {
+  headers: {
+    Location: "/dogs",
+  },
+  statusCode: 302,
+  body: false,
+};
+
+// * PUT/PATCH
+request = {
+  method: "POST",
+  URL: "/resource/:id/edit",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  body: {
+    key: true,
+  },
+};
+
+// * DELETE
+request = {
+  method: "POST",
+  URL: "/resource/:id/delete",
+};
+
+
+
+
+
+// Endpoints follow restful convention
+// * GET /resource
+// * POST /resource/
+// * GET /resource/:id
+// * PUT /resource/:id
+// * PATCH /resource/:id
+// * DELETE /resource/:id/delete
+
+
+
+// * JSON Web server
+// GET/POST/PUT/PATCH/DELETE
+
+
+// JSON Web Server
+// * REQUEST
+// * GET
+request = {
+  method: "GET",
+  URL: "/resource",
+  headers: false,
+  body: false,
+};
+
+response = {
+  headers: {
+    "Content-Type": "application/json",
+  },
+  statusCode: 200,
+  body: true,
+};
+
+// * POST
+request = {
+  method: "POST",
+  URL: "/resource",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: {
+    key: true,
+  },
+};
+
+response = {
+  headers: {
+    Location: "/dogs",
+  },
+  statusCode: 302,
+  body: false,
+};
+
+// * PUT/PATCH
+request = {
+  method: "PUT",
+  URL: "/resource/:id",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: {
+    key: true,
+  },
+};
+
+// * DELETE
+request = {
+  method: "DELETE",
+  URL: "/resource/:id",
+};
