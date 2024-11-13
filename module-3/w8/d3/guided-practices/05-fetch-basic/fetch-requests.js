eb10aa37949e14289347b46207e977cc49e21653
brandon <brandonlaursen95@yahoo.com>
2024-11-13 16:30:47 -0500

    fetch

--- module-3/w8/d3/guided-practices/05-fetch-basic/fetch-requests.js
+++ module-3/w8/d3/guided-practices/05-fetch-basic/fetch-requests.js
@@ -5,22 +5,26 @@ Paste your code for fetch requests here once you finish each task.
 
 /* =============== 1. Print the status code of the response =============== */
 
-// Your code here 
+(async function(){
+  const res = await fetch('/products');
+  const data = await res.text();
+     console.log(data)
+ })();
 
-
+ fetch('/products').then((res) => res.text()).then((data) => console.log(data))
 
 /* ====== 2. Print true if the status of the response was successful ====== */
 
-// Your code here 
+// Your code here
 
 
 
 /* =================== 3. Print the Content-Type Header =================== */
 
-// Your code here 
+// Your code here
 
 
 
 /* ============== 4. Print the body of the response as text =============== */
 
-// Your code here 
+// Your code here
