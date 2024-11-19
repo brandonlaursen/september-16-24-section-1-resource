3d721be2fb8ab3768cc62686526ffb0d6eb6841d
brandon <brandonlaursen95@yahoo.com>
2024-11-19 12:00:45 -0500

    nodelist vs html collection

--- module-3/w9/d2/guided-practices/01-nodelist-vs-htmlcollection/assets/js/html-collection.js
+++ module-3/w9/d2/guided-practices/01-nodelist-vs-htmlcollection/assets/js/html-collection.js
@@ -1,11 +1,35 @@
+
+
+
+
+
+
+
+
+
 export default () => {
+
   const bodyChildElements = document.body.children; // HTMLCollection [div]
   // console.log(bodyChildElements);
 
+  // [...bodyChildElements].forEach(ele => console.log(ele));
+
   const div = bodyChildElements[0];
+  // console.log("div:", div);
 
   const divChildElements = div.children; // HTMLCollection [span]
+  // console.log("divChildElements:", divChildElements);
+
+
   const helloWorld = div.innerText; // Hello World! Yes!    <-- NOT Hello World!
+  // console.log("helloWorld:", helloWorld);
+
+  // console.log(div);
+  // div.innerHTML += '<span>TEST </span> test 2';
+
+  // console.log(div);
+
   const span = divChildElements[0]; // <span>Yes!</span>
+  // console.log("span:", span);
   // debugger
 };
--- module-3/w9/d2/guided-practices/01-nodelist-vs-htmlcollection/assets/js/node-list.js
+++ module-3/w9/d2/guided-practices/01-nodelist-vs-htmlcollection/assets/js/node-list.js
@@ -1,11 +1,30 @@
+
+
+
+
+
+
+
+
 export default () => {
   const bodyChildNodes = document.body.childNodes; // NodeList [text, div, text]
+  console.log("bodyChildNodes:", bodyChildNodes);
   // console.log(bodyChildNodes);
 
+  bodyChildNodes.forEach(ele => console.log(ele));
+
   const div = bodyChildNodes[1]; // NOT bodyChildNodes[0]
+  // console.log("div:", div);
+
 
   const divChildNodes = div.childNodes; // NodeList [text, span, text]
+  console.log("divChildNodes:", divChildNodes);
+
+
   const helloWorld = divChildNodes[0].textContent; // Hello World!\n
+  console.log("helloWorld:", helloWorld);
+
   const span = divChildNodes[1]; // <span>Yes!</span>
+  console.log("span:", span);
   // debugger
 };
