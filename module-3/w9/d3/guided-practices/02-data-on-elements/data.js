4e50d134179da0c3143136f18e608f1f20dd4d62
brandon <brandonlaursen95@yahoo.com>
2024-11-20 13:32:57 -0500

    lecture

--- module-3/w9/d3/guided-practices/02-data-on-elements/data.js
+++ module-3/w9/d3/guided-practices/02-data-on-elements/data.js
@@ -1,1 +1,43 @@
-// Your code here 
+// Your code here
+
+
+
+
+
+console.log('first')
+
+window.addEventListener('DOMContentLoaded', (e) => {
+
+
+  const addButton = document.getElementById('add');
+
+
+  addButton.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    console.log('second')
+    const nameInput = document.getElementById('name');
+
+    const type = document.getElementById('type')
+
+    const ul = document.getElementById('shopping-list');
+
+    const newLi = document.createElement('li');
+
+    newLi.innerText = nameInput.value
+
+    newLi.dataset.type = type.value
+
+
+    ul.appendChild(newLi);
+
+    
+
+
+  })
+
+
+
+
+})
