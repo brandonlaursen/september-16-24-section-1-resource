d7e0057befaf62f753acf6b2d727faa8450663e0
brandon <brandonlaursen95@yahoo.com>
2024-11-25 17:26:42 -0500

    done with lecture

--- module-4/W10/D2/04-express-environment-variables/server/app.js
+++ module-4/W10/D2/04-express-environment-variables/server/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-// Your code here 
+
+require('dotenv').config();
+
+
 
 app.get('/', (req, res) => {
     res.send(process.env.SECRET_MESSAGE);
+    res.send('i dont want anyone to see this');
 });
 
 const port = process.env.PORT;
