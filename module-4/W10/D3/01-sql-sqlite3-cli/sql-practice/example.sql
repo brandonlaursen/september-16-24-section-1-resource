d12342b6d8553739502e45ad459d76b342d17ddb
brandon <brandonlaursen95@yahoo.com>
2024-11-26 12:48:45 -0500

    1 + 2

--- module-4/W10/D3/01-sql-sqlite3-cli/sql-practice/example.sql
+++ module-4/W10/D3/01-sql-sqlite3-cli/sql-practice/example.sql
@@ -1,8 +1,24 @@
+
+.mode table
+
 DROP TABLE IF EXISTS samples;
 
 CREATE TABLE samples (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);
 
-INSERT INTO samples (name) 
+INSERT INTO samples (name)
 VALUES ('You found me!'), ('Yippee!!'), ('Yay!!!');
 
 SELECT * FROM samples;
+
+-- sqlite3 databaseName.db
+-- .open databaseName.db
+-- .read sqlFile.sql
+-- .database
+-- .table
+-- .schema
+-- .exit
+-- .help
+
+-- SQL
+-- CREATE TABLE
+-- INSERT INTO
