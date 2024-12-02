fc6f9289fff72e49dbcbdd9853f997f290285ffa
brandon <brandonlaursen95@yahoo.com>
2024-12-02 16:39:38 -0500

    lecture notes

--- module-4/W11/D1/guided-practices/aa11-sql-query-improvement/sql-practice/improve-sql-queries.sql
+++ module-4/W11/D1/guided-practices/aa11-sql-query-improvement/sql-practice/improve-sql-queries.sql
@@ -1,12 +1,18 @@
 ----------
--- Step 0 - Create a Query 
+-- Step 0 - Create a Query
 ----------
 -- Query: Select all cats that have a toy with an id of 5
-
-    -- Your code here 
-
+-- .mode table
+-- SELECT * FROM cats
+-- JOIN cat_toys
+-- ON cats.id = cat_toys.cat_id
+-- JOIN toys
+-- ON toys.id = cat_toys.toy_id
+-- WHERE cat_toys.toy_id = 5;
 -- Paste your results below (as a comment):
-
+-- | 4002 | Rachele | Maroon   | Foldex Cat | 4509  | 4002   | 5      | 5  | Shiny Mouse | Blue  | 7     |
+-- | 31   | Rodger  | Lavender | Oregon Rex | 10008 | 31     | 5      | 5  | Shiny Mouse | Blue  | 7     |
+-- | 77   | Jamal   | Orange   | Sam Sawet  | 10051 | 77     | 5      | 5  | Shiny Mouse | Blue  | 7
 
 
 
@@ -15,10 +21,20 @@
 ----------
 -- Query:
 
-    -- Your code here 
+-- .mode table
+-- EXPLAIN QUERY PLAN
+-- SELECT * FROM cats
+-- JOIN cat_toys
+-- ON cats.id = cat_toys.cat_id
+-- JOIN toys
+-- ON toys.id = cat_toys.toy_id
+-- WHERE cat_toys.toy_id = 5;
 
 -- Paste your results below (as a comment):
-
+-- QUERY PLAN
+-- |--SEARCH toys USING INTEGER PRIMARY KEY (rowid=?)
+-- |--SCAN cat_toys
+-- `--SEARCH cats USING INTEGER PRIMARY KEY (rowid=?)
 
 -- What do your results mean?
 
@@ -35,10 +51,17 @@
 ----------
 -- Query (to be used in the sqlite CLI):
 
-    -- Your code here 
-
+-- .mode table
+-- .timer on
+-- EXPLAIN QUERY PLAN
+-- SELECT * FROM cats
+-- JOIN cat_toys
+-- ON cats.id = cat_toys.cat_id
+-- JOIN toys
+-- ON toys.id = cat_toys.toy_id
+-- WHERE cat_toys.toy_id = 5;
 -- Paste your results below (as a comment):
-
+-- Run Time: real 0.001 user 0.000092 sys 0.001092
 
 
 
@@ -48,10 +71,15 @@
 
 -- Create index:
 
-    -- Your code here 
+-- CREATE INDEX <index_name> ON <table_name> (<list_of_cols, ...>);
+DROP INDEX index_cat_toys_toy_id;
+
+CREATE INDEX index_cat_toys_toy_id ON cat_toys(toy_id);
+
+.indexes
 
 -- Analyze Query:
-    -- Your code here 
+    -- Your code here
 
 -- Paste your results below (as a comment):
 
@@ -68,10 +96,24 @@
 ----------
 -- Query (to be used in the sqlite CLI):
 
-    -- Your code here 
+
+.mode table
+.timer on
+EXPLAIN QUERY PLAN
+SELECT * FROM cats
+JOIN cat_toys
+ON cats.id = cat_toys.cat_id
+JOIN toys
+ON toys.id = cat_toys.toy_id
+WHERE cat_toys.toy_id = 5;
 
 -- Paste your results below (as a comment):
 
+--with index
+Run Time: real 0.000 user 0.000032 sys 0.000005
+
+-- without index
+Run Time: real 0.001 user 0.000092 sys 0.001092
 
 -- Analyze Results:
     -- Are you still getting the correct query results?
