ee77c95c610766fb1374922388f3a68bbca7fb0b
brandon <brandonlaursen95@yahoo.com>
2024-12-03 18:29:30 -0500

    end of lecture

---
+++ module-4/W11/D2/07-sequelize-modify-table-migrations/server/db/migrations/20241203231553-add-unique-constraint-to-color-names.js
@@ -0,0 +1,31 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.addConstraint("Colors", {
+      fields: ["name"],
+      type: "unique",
+      name: "add-unique-constraint-to-color-names",
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeConstraint(
+      "Colors",
+      "add-unique-constraint-to-color-names"
+    );
+  },
+};
