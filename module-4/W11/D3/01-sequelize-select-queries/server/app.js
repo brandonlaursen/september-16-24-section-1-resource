d542c1bfe752a58f1776ba312c05691696fdf5d0
brandon <brandonlaursen95@yahoo.com>
2024-12-04 11:30:15 -0500

    notes;

--- module-4/W11/D3/01-sequelize-select-queries/server/app.js
+++ module-4/W11/D3/01-sequelize-select-queries/server/app.js
@@ -21,7 +21,13 @@ app.use(express.json());
 app.get('/puppies', async (req, res, next) => {
     let allPuppies;
 
-    // Your code here 
+   allPuppies = await Puppy.findAll({
+    order: [['name', 'ASC']],
+    where: {
+        name: 'Callie'
+    }
+   });
+   console.log(allPuppies)
 
     res.json(allPuppies);
 });
@@ -33,7 +39,7 @@ app.get('/puppies', async (req, res, next) => {
 app.get('/puppies/chipped', async (req, res, next) => {
     let chippedPuppies;
 
-    // Your code here 
+    // Your code here
 
     res.json(chippedPuppies);
 });
@@ -44,8 +50,8 @@ app.get('/puppies/chipped', async (req, res, next) => {
 // Finding one record by attribute
 app.get('/puppies/name/:name', async (req, res, next) => {
     let puppyByName;
-    
-    // Your code here 
+
+    // Your code here
 
     res.json(puppyByName);
 })
@@ -56,8 +62,8 @@ app.get('/puppies/name/:name', async (req, res, next) => {
 // WHERE clause with a comparison
 app.get('/puppies/shepherds', async (req, res, next) => {
     let shepherds;
-    
-    // Your code here 
+
+    // Your code here
 
     res.json(shepherds);
 })
@@ -68,8 +74,8 @@ app.get('/puppies/shepherds', async (req, res, next) => {
 // WHERE clause with multiple attributes and comparisons
 app.get('/puppies/tinybabies', async (req, res, next) => {
     let tinyBabyPuppies;
-    
-    // Your code here 
+
+    // Your code here
 
     res.json(tinyBabyPuppies);
 })
@@ -80,9 +86,9 @@ app.get('/puppies/tinybabies', async (req, res, next) => {
 // Finding one record by primary key
 app.get('/puppies/:id', async (req, res, next) => {
     let puppyById;
-    
-    // Your code here 
-    
+
+    // Your code here
+
     res.json(puppyById);
 });
 
