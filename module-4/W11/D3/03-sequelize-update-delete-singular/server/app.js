e3852031ed82e24720a37f90fa95561abd00b8db
brandon <brandonlaursen95@yahoo.com>
2024-12-04 13:56:57 -0500

    notes

--- module-4/W11/D3/03-sequelize-update-delete-singular/server/app.js
+++ module-4/W11/D3/03-sequelize-update-delete-singular/server/app.js
@@ -20,13 +20,49 @@ app.get('/puppies', async (req, res, next) => {
 
 // STEP 1: Update a puppy by id
 app.put('/puppies/:puppyId', async (req, res, next) => {
-    // Your code here 
+
+    // extract the data the req.body
+    // find the puppy in the database
+    // console.log(req.params.puppyId)
+    const puppy = await Puppy.findByPk(req.params.puppyId);
+    // console.log(puppy)
+    // key into puppy objects and updates values
+    // console.log(req.body)
+    const {ageYrs, weightLbs, microchipped} = req.body;
+    // console.log('==>', ageYrs)
+
+    if(ageYrs !== undefined) puppy.ageYrs = ageYrs;
+    if(weightLbs !== undefined) puppy.weightLbs = weightLbs;
+    if(microchipped !== undefined) puppy.microchipped = microchipped;
+
+    // console.log(puppy)
+    // save the puppy
+    await puppy.save();
+
+    // .save()
+    res.json({
+        message:'puppy was updated',
+        puppy
+    })
+
 })
 
 
 // STEP 2: Delete a puppy by id
 app.delete('/puppies/:puppyId', async (req, res, next) => {
-    // Your code here 
+    // Your code here
+
+    // find the puppy - findByPk
+    // destroy()
+    const puppy = await Puppy.findByPk(req.params.puppyId);
+
+    await puppy.destroy();
+
+    res.json({
+        message:'puppy was ADOPTED',
+        puppy
+    })
+
 })
 
 
